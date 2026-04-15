d8a1fba5552da696cfe14b894fd3cc204fe93467
Ковригин Владислав Сергеевич <v.kovrigin@npo.lan>
2026-04-15 15:55:50 +0300

    Initial Commit!

---
+++ src/main/java/com/vladko/autoshopauth/AutoshopAuthApplication.java
@@ -0,0 +1,13 @@
+package com.vladko.autoshopauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoshopAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoshopAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vladko/autoshopauth/AutoshopAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladko.autoshopauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoshopAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
