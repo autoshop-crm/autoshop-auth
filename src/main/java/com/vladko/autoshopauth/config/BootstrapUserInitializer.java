45dbb5a09f63eaabdb685cedd4667a88ce4fca90
Ковригин Владислав Сергеевич <v.kovrigin@npo.lan>
2026-05-24 00:22:37 +0300

    Enable bootstrap CRM admin creation

--- src/main/java/com/vladko/autoshopauth/config/BootstrapUserInitializer.java
+++ src/main/java/com/vladko/autoshopauth/config/BootstrapUserInitializer.java
@@ -12,13 +12,11 @@ import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
-@Profile({"dev", "test", "local"})
 @RequiredArgsConstructor
 public class BootstrapUserInitializer implements ApplicationRunner {
 
